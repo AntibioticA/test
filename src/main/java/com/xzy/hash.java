d05f17b8d77a5dc2eb6589d8f170a48d3a27dff6
Xzy <1564523074@qq.com>
2023-03-19 20:18:32 +0800

    first commit

---
+++ src/main/java/com/xzy/hash.java
@@ -0,0 +1,55 @@
+package com.xzy;
+
+import java.util.Objects;
+
+public class hash {
+    public static void main(String[] args) {
+
+//    Student student1=new Student("xiaoming");
+//    Student student2=new Student("xiaoming");
+//    System.out.println(student1.hashCode());
+//    System.out.println(student2.hashCode());
+        System.out.println("Aa".hashCode());
+        System.out.println("BB".hashCode());
+    }
+}
+
+class Student{
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
+
+
+
